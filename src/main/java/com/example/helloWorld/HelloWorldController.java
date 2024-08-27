508272d7d66df2a8dfe89a12d4053b400fa895ee
szysza <techdevpassion@op.pl>
2024-08-27 17:50:12 +0200

    Added JavaDocs

--- src/main/java/com/example/helloWorld/HelloWorldController.java
+++ src/main/java/com/example/helloWorld/HelloWorldController.java
@@ -6,11 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+    /**
+     * HelloWorld method
+     * @return
+     */
     @GetMapping("/hello")
     public String hello(){
         return "Hello World! Julia ";
     }
 
+    /**
+     * Info method
+     * @return
+     */
     @GetMapping("/info")
     public String info(){
         return "Informacja.";
